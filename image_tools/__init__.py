7e70a1ca854c424304a3bcfef37e6d58104f2385
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-06-10 11:39:26 -0400

    add astropy init

--- image_tools/__init__.py
+++ image_tools/__init__.py
@@ -1,10 +1,24 @@
-import radialprofile
-from radialprofile import azimuthalAverage,azimuthalAverageBins,radialAverage,radialAverageBins
-from drizzle import drizzle
-import downsample
-from downsample import downsample,downsample_1d,downsample_cube,downsample_axis
-#from cross_correlation_shifts import cross_correlation_shifts_FITS,cross_correlation_shifts
-# these have been moved to their own repository: https://github.com/keflavich/image_registration
-#from register_images import register_images,dftregistration
-#from registration_testing import register_noise_test
-#from chi2_shifts import chi2_shift
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+"""
+This is an Astropy affiliated package.
+"""
+
+# Affiliated packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+from ._astropy_init import *
+# ----------------------------------------------------------------------------
+
+# For egg_info test builds to pass, put package imports here.
+if not _ASTROPY_SETUP_:
+    import radialprofile
+    from radialprofile import azimuthalAverage,azimuthalAverageBins,radialAverage,radialAverageBins
+    from drizzle import drizzle
+    import downsample
+    from downsample import downsample,downsample_1d,downsample_cube,downsample_axis
+    #from cross_correlation_shifts import cross_correlation_shifts_FITS,cross_correlation_shifts
+    # these have been moved to their own repository: https://github.com/keflavich/image_registration
+    #from register_images import register_images,dftregistration
+    #from registration_testing import register_noise_test
+    #from chi2_shifts import chi2_shift
